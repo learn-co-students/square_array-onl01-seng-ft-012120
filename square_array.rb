4392bafb1663d9a02938d9aa4f47933fefe717ee
Roberto Livi <robertomlivi@outlook.com>
2020-01-22 22:19:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+nums_array = [1, 2, 3]
+def square_array(nums_array)
+  new_array = []
+  
+  nums_array. each do |num|
+    num**2
+  end
+  
+  return new_array
 end
+
+square_array
